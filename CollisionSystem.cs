a252055c92f15dd97a70cc28ec3b549db2f1106b
Warwick Stone <uozu.aho@gmail.com>
2019-03-24 22:22:10 +1100

    add particle image, document plan of attack

--- CollisionSystem.cs
+++ CollisionSystem.cs
@@ -26,44 +26,49 @@ namespace particles
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  */
-public class CollisionSystem
-{
-    private const double HZ = 0.5;    // number of redraw events per clock tick
 
-    private MinPQ<Event> pq;          // the priority queue
-    private double t  = 0.0;          // simulation clock time
-    private Particle[] particles;     // the array of particles
+    public class CollisionSystem
+    {
+        private const double HZ = 0.5; // number of redraw events per clock tick
 
-    /**
-     * Initializes a system with the specified collection of particles.
-     * The individual particles will be mutated during the simulation.
-     *
-     * @param  particles the array of particles
-     */
-    public CollisionSystem(Particle[] particles) {
-        this.particles = particles.Select(p => p.Clone()).ToArray(); // defensive copy
-    }
+        private MinPQ<Event> pq; // the priority queue
+        private double t = 0.0; // simulation clock time
+        private Particle[] particles; // the array of particles
 
-    // updates priority queue with all new events for particle a
-    private void predict(Particle a, double limit) {
-        if (a == null) return;
-
-        // particle-particle collisions
-        for (int i = 0; i < particles.Length; i++) {
-            double dt = a.timeToHit(particles[i]);
-            if (t + dt <= limit)
-                pq.Push(new Event(t + dt, a, particles[i]));
+        /**
+         * Initializes a system with the specified collection of particles.
+         * The individual particles will be mutated during the simulation.
+         *
+         * @param  particles the array of particles
+         */
+        public CollisionSystem(Particle[] particles)
+        {
+            this.particles = particles.Select(p => p.Clone()).ToArray(); // defensive copy
         }
 
-        // particle-wall collisions
-        double dtX = a.timeToHitVerticalWall();
-        double dtY = a.timeToHitHorizontalWall();
-        if (t + dtX <= limit) pq.Push(new Event(t + dtX, a, null));
-        if (t + dtY <= limit) pq.Push(new Event(t + dtY, null, a));
-    }
+        // updates priority queue with all new events for particle a
+        private void predict(Particle a, double limit)
+        {
+            if (a == null) return;
 
-    // redraw all particles
-    private void redraw(double limit) {
+            // particle-particle collisions
+            for (int i = 0; i < particles.Length; i++)
+            {
+                double dt = a.timeToHit(particles[i]);
+                if (t + dt <= limit)
+                    pq.Push(new Event(t + dt, a, particles[i]));
+            }
+
+            // particle-wall collisions
+            double dtX = a.timeToHitVerticalWall();
+            double dtY = a.timeToHitHorizontalWall();
+            if (t + dtX <= limit) pq.Push(new Event(t + dtX, a, null));
+            if (t + dtY <= limit) pq.Push(new Event(t + dtY, null, a));
+        }
+
+        // redraw all particles
+        private void redraw(double limit)
+        {
 //        StdDraw.clear();
 //        for (int i = 0; i < particles.length; i++) {
 //            particles[i].draw();
@@ -73,118 +78,133 @@ public class CollisionSystem
 //        if (t < limit) {
 //            pq.insert(new Event(t + 1.0 / HZ, null, null));
 //        }
-    }
+        }
 
-    private class EventComparer : IComparer<Event>
-    {
-        public int Compare(Event x, Event y)
+        private class EventComparer : IComparer<Event>
         {
-            return x.CompareTo(y);
+            public int Compare(Event x, Event y)
+            {
+                return x.CompareTo(y);
+            }
         }
-    }
-      
-    /**
-     * Simulates the system of particles for the specified amount of time.
-     *
-     * @param  limit the amount of time
-     */
-    public void simulate(double limit) {
-        
-        // initialize PQ with collision events and redraw event
-        pq = new MinPQ<Event>(new EventComparer());
-        for (int i = 0; i < particles.Length; i++) {
-            predict(particles[i], limit);
-        }
-        pq.Push(new Event(0, null, null));        // redraw event
 
+        /**
+         * Simulates the system of particles for the specified amount of time.
+         *
+         * - predict next collision for all particles, push collisions to queue
+         * - push redraw to queue
+         * - while queue is not empty
+         *     - pop from queue
+         *     - update all particles to current time
+         *     - recalculate next collisions for event popped off queue, push to queue
+         *
+         * 
+         * @param  limit the amount of time
+         */
+        public void simulate(double limit)
+        {
 
-        // the main event-driven simulation loop
-        while (!pq.IsEmpty) { 
-
-            // get impending event, discard if invalidated
-            Event e = pq.Pop();
-            if (!e.isValid()) continue;
-            Particle a = e.a;
-            Particle b = e.b;
-
-            // physical collision, so update positions, and then simulation clock
+            // initialize PQ with collision events and redraw event
+            pq = new MinPQ<Event>(new EventComparer());
             for (int i = 0; i < particles.Length; i++)
-                particles[i].move(e.time - t);
-            t = e.time;
+            {
+                predict(particles[i], limit);
+            }
 
-            // process event
-            if      (a != null && b != null) a.bounceOff(b);              // particle-particle collision
-            else if (a != null && b == null) a.bounceOffVerticalWall();   // particle-wall collision
-            else if (a == null && b != null) b.bounceOffHorizontalWall(); // particle-wall collision
-            else if (a == null && b == null) redraw(limit);               // redraw event
-
-            // update the priority queue with new collisions involving a or b
-            predict(a, limit);
-            predict(b, limit);
-        }
-    }
+            pq.Push(new Event(0, null, null)); // redraw event
 
 
-   /***************************************************************************
-    *  An event during a particle collision simulation. Each event contains
-    *  the time at which it will occur (assuming no supervening actions)
-    *  and the particles a and b involved.
-    *
-    *    -  a and b both null:      redraw event
-    *    -  a null, b not null:     collision with vertical wall
-    *    -  a not null, b null:     collision with horizontal wall
-    *    -  a and b both not null:  binary collision between a and b
-    *
-    ***************************************************************************/
-    private class Event : IComparable<Event> {
-        public readonly double time;         // time that event is scheduled to occur
-        public readonly Particle a, b;       // particles involved in event, possibly null
-        public readonly int countA, countB;  // collision counts at event creation
-        
-        // create a new event to occur at time t involving a and b
-        public Event(double t, Particle a, Particle b) {
-            this.time = t;
-            this.a    = a;
-            this.b    = b;
-            if (a != null) countA = a.count;
-            else           countA = -1;
-            if (b != null) countB = b.count;
-            else           countB = -1;
+            // the main event-driven simulation loop
+            while (!pq.IsEmpty)
+            {
+
+                // get impending event, discard if invalidated
+                Event e = pq.Pop();
+                if (!e.isValid()) continue;
+                Particle a = e.a;
+                Particle b = e.b;
+
+                // physical collision, so update positions, and then simulation clock
+                for (int i = 0; i < particles.Length; i++)
+                    particles[i].move(e.time - t);
+                t = e.time;
+
+                // process event
+                if (a != null && b != null) a.bounceOff(b); // particle-particle collision
+                else if (a != null && b == null) a.bounceOffVerticalWall(); // particle-wall collision
+                else if (a == null && b != null) b.bounceOffHorizontalWall(); // particle-wall collision
+                else if (a == null && b == null) redraw(limit); // redraw event
+
+                // update the priority queue with new collisions involving a or b
+                predict(a, limit);
+                predict(b, limit);
+            }
         }
 
-        // compare times when two events will occur
-        public int CompareTo(Event that)
+        /// <summary>
+        /// An event during a particle collision simulation. Each event contains
+        /// the time at which it will occur (assuming no supervening actions)
+        /// and the particles a and b involved.
+        /// </summary>
+        /// <remarks>
+        ///  - a and b both null:      redraw event
+        ///  - a null, b not null:     collision with vertical wall
+        ///  - a not null, b null:     collision with horizontal wall
+        ///  - a and b both not null:  binary collision between a and b
+        /// </remarks>
+        private class Event : IComparable<Event>
         {
-            return time < that.time ? -1 : time > that.time ? 1 : 0;
-        }
-        
-        // has any collision occurred between when event was created and now?
-        public bool isValid() {
-            if (a != null && a.count != countA) return false;
-            if (b != null && b.count != countB) return false;
-            return true;
-        }
-    }
+            public readonly double time; // time that event is scheduled to occur
+            public readonly Particle a, b; // particles involved in event, possibly null
+            public readonly int countA, countB; // collision counts at event creation
 
-    /**
-     * Unit tests the {@code CollisionSystem} data type.
-     * Reads in the particle collision system from a standard input
-     * (or generates {@code N} random particles if a command-line integer
-     * is specified); simulates the system.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
+            // create a new event to occur at time t involving a and b
+            public Event(double t, Particle a, Particle b)
+            {
+                this.time = t;
+                this.a = a;
+                this.b = b;
+                if (a != null) countA = a.count;
+                else countA = -1;
+                if (b != null) countB = b.count;
+                else countB = -1;
+            }
+
+            // compare times when two events will occur
+            public int CompareTo(Event that)
+            {
+                return time < that.time ? -1 : time > that.time ? 1 : 0;
+            }
+
+            // has any collision occurred between when event was created and now?
+            public bool isValid()
+            {
+                if (a != null && a.count != countA) return false;
+                if (b != null && b.count != countB) return false;
+                return true;
+            }
+        }
+
+        /**
+         * Unit tests the {@code CollisionSystem} data type.
+         * Reads in the particle collision system from a standard input
+         * (or generates {@code N} random particles if a command-line integer
+         * is specified); simulates the system.
+         *
+         * @param args the command-line arguments
+         */
+        public static void main(String[] args)
+        {
 
 //        StdDraw.setCanvasSize(600, 600);
 
-        // enable double buffering
+            // enable double buffering
 //        StdDraw.enableDoubleBuffering();
 
-        // create collision system and simulate
+            // create collision system and simulate
 //        CollisionSystem system = new CollisionSystem(particles);
 //        system.simulate(10000);
+        }
+
     }
-      
-}
 }
