1f6f59eaaf21c716b3850514ee93cdb66191bf17
Warwick Stone <uozu.aho@gmail.com>
2019-03-28 21:58:09 +1100

    extract CollisionEvent

--- particles/CollisionSystem.cs
+++ particles/CollisionSystem.cs
@@ -115,44 +115,5 @@ namespace particles
                 return x.Time < y.Time ? -1 : x.Time > y.Time ? 1 : 0;
             }
         }
-
-        /// <summary>
-        /// An event during a particle collision simulation. Each event contains
-        /// the time at which it will occur (assuming no supervening actions)
-        /// and the particles a and b involved.
-        /// </summary>
-        /// <remarks>
-        ///  - a null, b not null:     collision with vertical wall
-        ///  - a not null, b null:     collision with horizontal wall
-        ///  - a and b both not null:  binary collision between a and b
-        /// </remarks>
-        private class CollisionEvent
-        {
-            public readonly double Time;
-            public readonly Particle A, B;
-            private readonly int _countA; // collision counts at event creation
-            private readonly int _countB; // collision counts at event creation
-
-            public CollisionEvent(double timeSeconds, Particle a, Particle b)
-            {
-                Time = timeSeconds;
-                A = a;
-                B = b;
-                if (a != null) _countA = a.NumCollisions;
-                else _countA = -1;
-                if (b != null) _countB = b.NumCollisions;
-                else _countB = -1;
-            }
-
-            /// <summary>
-            /// False if either particle in the event has collided since this event was created
-            /// </summary>
-            public bool IsValid()
-            {
-                if (A != null && A.NumCollisions != _countA) return false;
-                if (B != null && B.NumCollisions != _countB) return false;
-                return true;
-            }
-        }
     }
 }
