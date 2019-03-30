fc5dd22a7d29bdee2e15ea34c0497a30ff282166
Warwick Stone <uozu.aho@gmail.com>
2019-03-30 23:19:38 +1100

    extract metric classes

---
+++ particles.monogame/FpsMetric.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace particles.monogame
+{
+    internal class FpsMetric
+    {
+        private readonly long[] _frameTimes = new long[100];
+        private readonly Stopwatch _stopwatch;
+        private int _idx = 0;
+
+        public FpsMetric()
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnFrame()
+        {
+            _frameTimes[_idx++] = _stopwatch.ElapsedMilliseconds;
+            if (_idx == _frameTimes.Length)
+                _idx = 0;
+        }
+
+        public double Fps()
+        {
+            var latestIdx = _idx - 1;
+            if (latestIdx < 0) latestIdx = _frameTimes.Length - 1;
+
+            var totalTimeMs = _frameTimes[latestIdx] - _frameTimes[_idx];
+
+            if (totalTimeMs < double.Epsilon) return 0;
+
+            return (_frameTimes.Length * 1000.0) / totalTimeMs;
+        }
+    }
+}
---
+++ particles.monogame/FpsMetricRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace particles.monogame
+{
+    internal class FpsMetricRenderer
+    {
+        private readonly SpriteFont _font;
+        private readonly Vector2 _pos;
+
+        public FpsMetricRenderer(SpriteFont font, Vector2 pos)
+        {
+            _font = font;
+            _pos = pos;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, FpsMetric metric)
+        {
+            var fps = metric.Fps();
+            spriteBatch.DrawString(_font, $"fps: {fps:0.##}", _pos, Color.Red);
+        }
+    }
+}
